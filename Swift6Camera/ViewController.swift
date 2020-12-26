e7f9261d62a057820b6fef1ec6bf8186eb283f77
div-antk <51283468+owaii@users.noreply.github.com>
2020-12-26 14:42:01 +0900

    Initial Commit

---
+++ Swift6Camera/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift6Camera
+//
+//  Created by Takuya Ando on 2020/12/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
